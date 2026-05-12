8ab7aa44fa62cfa2e641b1069c7eb9447eaf5688
snehapahuja <snehapahuja604@gmail.com>
2026-05-12 19:03:15 +0530

    dto created

---
+++ PetManagementSystem.Api/DTOs/VaccinationDto.cs
@@ -0,0 +1,28 @@
+﻿namespace PetManagementSystem.Api.DTOs
+{
+    public class VaccinationDto
+    {
+        public int VaccinationId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public bool? Available { get; set; }
+    }
+
+    public class CreateVaccinationDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public bool? Available { get; set; }
+    }
+
+    public class UpdateVaccinationDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public bool? Available { get; set; }
+    }
+
+}
